2d614811a9ef75f9eafe7dc7312e9a91e9a4344e
Ryan <ryan@ohmgeek.co.uk>
2017-09-24 15:15:29 +0100

    Client side scripts, to get the basics working

--- client/script.js
+++ client/script.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
+  var BASE_URL = window.location.origin;
 
-  
+  var tryToUpdate = function() {
+    // GET the next message.
+    $.getJSON(BASE_URL + '/fetchmsg', function(resp) {
+      var message = resp.message;
 
+      if(message) {
+        $('#message').innerText = message; // get the message.
+      }
+    });
 
+  }
 
+  window.setInterval(tryToUpdate, 10000); // try to update.
 });
